20981fc809fcead73f252ec4bf99564e1674e6c4
Abu Yusuf <yusuf_cse@yahoo.com>
2021-04-06 15:34:33 +0600

    closure and exception

---
+++ closures/invoke.php
@@ -0,0 +1,14 @@
+<?php
+
+class IsPositiveInt {
+
+	public function __invoke($value){
+		return((int)$value == $value && $value > 0);
+	}
+}
+
+$invoke = new IsPositiveInt();
+var_dump($invoke(5));
+var_dump($invoke(5.1));
+var_dump($invoke(-5));
+var_dump($invoke(5.0));
---
+++ exceptions/exceptions.php
@@ -0,0 +1,27 @@
+<?php 
+
+try{
+	processCC();
+}catch(Exception $e){
+	echo $e->getMessage();
+	echo "\n";
+	echo $e->getPrevious()->getMessage();
+}
+
+function processCC($numb = null, $zipCode = null){
+	
+	try{
+		validate($numb,$zipCode);
+	}catch(Exception $e){
+		throw new Exception("Invalid Inputs", null, $e);
+	}
+	echo "Processed";
+}
+
+function validate($numb,$zipCode){
+
+	if(is_null($numb)){
+		throw new Exception("No CC Number");		
+	}
+	
+}
